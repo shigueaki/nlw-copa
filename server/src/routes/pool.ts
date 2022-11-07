7aeae1274e8adb28acb1671dd0029c250d4ae386
shigueaki <44611671+shigueaki@users.noreply.github.com>
2022-11-07 16:50:12 -0300

    First Commit with project almost done

---
+++ server/src/routes/pool.ts
@@ -0,0 +1,188 @@
+import { FastifyInstance } from 'fastify';
+import { prisma } from '../lib/prisma';
+import {z} from 'zod';
+import ShortUniqueId from 'short-unique-id';
+import { authenticate } from '../plugins/authenticate';
+
+export async function poolRoutes(fastify : FastifyInstance) {
+    fastify.get('/pools/count', async () => {
+        const count = await prisma.pool.count();
+        return {count};
+    });
+
+    fastify.post('/pool', async (req, res) => {
+        const createPoolBody = z.object({
+            title: z.string(),
+        });
+
+        const { title } = createPoolBody.parse(req.body);
+
+        const generate = new ShortUniqueId({ length: 6 });
+        const code = String(generate()).toUpperCase();
+
+        try {
+            await req.jwtVerify();
+            await prisma.pool.create({
+                data: {
+                    title,
+                    code,
+                    ownerId: req.user.sub,
+
+                    participants: {
+                        create: {
+                            userId: req.user.sub,
+                        }
+                    }
+                }
+            });
+        } catch {
+            await prisma.pool.create({
+                data: {
+                    title,
+                    code,
+                }
+            });
+        }
+
+
+        return res.status(201).send(code);
+    });
+
+    fastify.post('/pools/join', { onRequest: [authenticate]}, async (req, res) => {
+        const joinPoolBody = z.object({
+            code: z.string(),
+        });
+
+        const { code } = joinPoolBody.parse(req.body);
+        const pool = await prisma.pool.findUnique({
+            where: {
+                code,
+            },
+            include: {
+                participants: {
+                    where: {
+                        userId : req.user.sub,
+                    }
+                }
+            }
+        });
+
+        if (!pool) {
+            return res.status(400).send({message: 'Pool not found.'});
+        }
+
+        if (pool.participants.length > 0) {
+            return res.status(400).send({message: 'You already joined this poll'});
+        }
+
+        if (!pool.ownerId) {
+            await prisma.pool.update({
+                where: {
+                    id: pool.id
+                },
+                data: {
+                    ownerId: req.user.sub,
+                }
+            });
+        }
+
+        await prisma.participant.create({
+            data: {
+                poolId: pool.id,
+                userId: req.user.sub,
+            }
+        });
+
+        return res.status(201).send();
+    });
+
+    fastify.get('/pools', { onRequest: [authenticate]}, async (req, res) => {
+        const pools = await prisma.pool.findMany({
+            where: {
+                participants: {
+                    some: {
+                        userId: req.user.sub,
+                    }
+                }
+            },
+            include: {
+                _count: {
+                    select: {
+                        participants: true,
+                    }
+                },
+                participants: {
+                    select: {
+                        id: true,
+
+                        user: {
+                            select: {
+                                avatarUrl: true,
+                            },
+                        },
+                    },
+                    take: 4,
+                },
+                owner: {
+                    select: {
+                        id: true,
+                        name: true,
+                    }
+                }
+            }
+        });
+
+        return { pools };
+    });
+
+    fastify.get('/pools/:poolId', { onRequest: [authenticate]}, async (req, res) => {
+        const getPoolParams = z.object({
+            poolId: z.string(),
+        });
+
+        const { poolId } = getPoolParams.parse(req.params);
+
+        const pool = await prisma.pool.findFirst({
+            include: {
+                _count: {
+                    select: {
+                        participants: true,
+                    }
+                },
+                participants: {
+                    select: {
+                        id: true,
+
+                        user: {
+                            select: {
+                                avatarUrl: true,
+                            },
+                        },
+                    },
+                    take: 4,
+                },
+                owner: {
+                    select: {
+                        id: true,
+                        name: true,
+                    }
+                }
+            },
+            where: {
+                id: poolId,
+                participants: {
+                    some: {
+                        userId: req.user.sub,
+                    }
+                }
+
+            },
+        });
+
+        if (!pool) {
+            return res.status(400).send({message: 'Pool not found.'});
+        }
+
+        return { pool };
+    });
+}
---
+++ server/src/server.ts
@@ -0,0 +1,35 @@
+import Fastify from 'fastify';
+import cors from '@fastify/cors';
+import jwt from '@fastify/jwt';
+import { poolRoutes } from './routes/pool';
+import { guessRoutes } from './routes/guess';
+import { userRoutes } from './routes/user';
+import { authRoutes } from './routes/auth';
+import { gameRoutes } from './routes/game';
+
+
+
+async function start() {
+    const fastify = Fastify({
+        logger: true
+    });
+
+    await fastify.register(cors, {
+        origin: true
+    });
+
+    //Em produção isso precisa ser uma variável ambiente
+    await fastify.register(jwt, {
+        secret: 'nlwcopa',
+    })
+
+    await fastify.register(authRoutes);
+    await fastify.register(poolRoutes);
+    await fastify.register(guessRoutes);
+    await fastify.register(userRoutes);
+    await fastify.register(gameRoutes);
+
+    await fastify.listen({ port: 3333, host: '0.0.0.0' });
+}
+
+start();
